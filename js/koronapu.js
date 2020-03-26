af6355ef938b43b86a1c67c172ac5b01602ad419
kiiski10 <pekka@pekka.pl>
2020-03-26 23:16:10 +0000

    summary in datapoint poppup, removed defaults from edit form fields

--- js/koronapu.js
+++ js/koronapu.js
@@ -208,7 +208,7 @@ function addAsInfectedMarker(i) {
 	})
 	.addTo(mymap)
 	L.marker(i["location"]).addTo(mymap)
-		.bindPopup("<div class='popup'><h3>" + i['name'] + " </h3>Potilas<br><br>" + i["description"] + "<br><button onclick='showMessagingPopup()'><img src='img/chat-bubble.png' alt='Lähetä viesti'></button></div>", 
+		.bindPopup("<div class='popup'><h3>" + i['name'] + " </h3>Potilas<br><br>" + i["summary"] + "<br><button onclick='showMessagingPopup()'><img src='img/chat-bubble.png' alt='Lähetä viesti'></button></div>", 
 		// TODO: Kuinka saada muuttuja i:n jutut htmlään (ehkä jqueryllä päivittää otsikon ja kuvauksen yms kun popup avataan) nyt unille :)
 		{ keepInView: true }
 	);
